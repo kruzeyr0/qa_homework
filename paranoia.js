5087a705391becbfb24541535bd51e42c65c0507
Andrii Katrych <katrich.andrey@ukr.net>
2026-01-15 23:20:17 +0200

    Fixed paranoia task with improvements to the functions and more test examples

--- paranoia.js
+++ paranoia.js
@@ -13,24 +13,37 @@ var arr = [
         userName:"Andrii",
         lastName:"",
         email:"andrii@mail.ru" // Нам такі не підходять
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:".test@gmail.com" //test email не може починатися з крапки
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"test@mail.ua@mail.ru" // test email не може містити більше одного символу "@"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"test@mail.ua" // test email не з потрібним доменом
+    },
+    {
+        userName:"test!!!",
+        lastName:"",
+        email:"test!!!@gmail.com" // test email не може містити спецсимволи, окрім крапки
     }
 ];
 
-function validateEmail(email) {
-    let validateChecker = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return validateChecker.test(String(email).toLowerCase());
+function validateEmailAndDomain(email) {
+    const emailRegex = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail\.com|yahoo\.com)$/;   // Only gmail.com and yahoo.com valid emails are allowed
+    return emailRegex.test(email);
 }
 
 function trustedEmails(arr) {
-    const trustedDomains = ['gmail.com', 'yahoo.com'];
-    return arr.map(userName => userName.email).filter(email => {
-        if (!validateEmail(email)) {
-            return false;
-        }
-        let domain = email.split('@')[1];
-        return trustedDomains.includes(domain);
-    });
-}
+    return arr.map(userName => userName.email).filter(email => validateEmailAndDomain(email));      // Filter only valid emails
+         }
 
 
 console.log(trustedEmails(arr));    // Expected output: [ 'test.test@gmail.com', 'dmitro.porohov@yahoo.com' ]
